283d11e315d1d4a94bfc367be4eba4bcae88361a
vickykathe <kathevela30@hotmail.com>
2019-10-17 21:03:01 -0500

    actualizar todo

--- colorsApp/app/src/androidTest/java/com/example/colorsapp/ExampleInstrumentedTest.java
+++ colorsApp/app/src/androidTest/java/com/example/colorsapp/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.colorsapp", appContext.getPackageName());
+        assertEquals("com.example.colorsApp", appContext.getPackageName());
     }
 }
